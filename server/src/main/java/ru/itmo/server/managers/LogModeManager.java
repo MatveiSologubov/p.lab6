701d30411a6fb8be4a17cb81808f589b5571c298
Matvei Sologubov <matthew.sologubov@gmail.com>
2025-05-15 17:57:24 +0300

    fix: Logging while in command mode is now working correctly

--- server/src/main/java/ru/itmo/server/managers/LogModeManager.java
+++ server/src/main/java/ru/itmo/server/managers/LogModeManager.java
@@ -1,24 +1,18 @@
 package ru.itmo.server.managers;
 
-import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.LoggerConfig;
 
 public class LogModeManager {
-    private static final LoggerContext ctx =
-            (LoggerContext) org.apache.logging.log4j.LogManager.getContext(false);
-    private static final Configuration config = ctx.getConfiguration();
-    private static final LoggerConfig loggerConfig =
-            config.getLoggerConfig("ru.itmo.server");
+    private static final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
 
     public static void enableConsoleLogging() {
-        loggerConfig.setLevel(Level.ALL);
-        ctx.updateLoggers(config);
+        System.setProperty("consoleLogLevel", "ALL");
+        ctx.reconfigure();
     }
 
     public static void disableConsoleLogging() {
-        loggerConfig.setLevel(Level.OFF);
-        ctx.updateLoggers(config);
+        System.setProperty("consoleLogLevel", "OFF");
+        ctx.reconfigure();
     }
 }
